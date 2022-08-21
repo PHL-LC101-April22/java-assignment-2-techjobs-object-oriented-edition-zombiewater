eca9df547f743f1b810c9df98a2597bdfa2f880b
Rafael Rodriguez Casanova <rafaelrodriguezcasanova@proton.me>
2022-08-21 19:18:55 -0400

    task two passes

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -13,6 +13,10 @@ public class Location {
         nextId++;
     }
 
+    public Location(String value) {
+        this();
+        this.value = value;
+    }
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
@@ -45,10 +49,6 @@ public class Location {
 
     public String getValue() {
         return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
     }
 
 }
